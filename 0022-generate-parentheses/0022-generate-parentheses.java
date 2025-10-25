7ec37278deb0e2c8d25470cf9589111f68eb0c6e
saipriya203 <battinisaipriya@gmail.com>
2025-10-25 12:43:54 +0530

    Time: 2 ms (43.78%), Space: 43.6 MB (24.63%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String current, int open, int close, int max) {
+        // If the current string is complete
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+
+        // If we can add an open parenthesis
+        if (open < max) {
+            backtrack(result, current + "(", open + 1, close, max);
+        }
+
+        // If we can add a close parenthesis
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, max);
+        }
+    }
+
+    // Local testing
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.generateParenthesis(3)); // ["((()))","(()())","(())()","()(())","()()()"]
+        System.out.println(sol.generateParenthesis(1)); // ["()"]
+    }
+}
