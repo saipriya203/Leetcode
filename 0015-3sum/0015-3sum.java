1a8c2e807320576b8dbf6dddffdcdabf44df9db8
saipriya203 <battinisaipriya@gmail.com>
2025-10-25 12:41:04 +0530

    Time: 30 ms (71.04%), Space: 51.3 MB (83.53%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sort the array first
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    // Skip duplicates for left
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    // Skip duplicates for right
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    // Local testing
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.threeSum(new int[]{-1,0,1,2,-1,-4})); // [[-1,-1,2],[-1,0,1]]
+        System.out.println(sol.threeSum(new int[]{0,1,1}));           // []
+        System.out.println(sol.threeSum(new int[]{0,0,0}));           // [[0,0,0]]
+    }
+}
